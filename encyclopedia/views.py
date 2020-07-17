e4bcd0a4e496acea1db9f00115c98b4967fee853
Adrià Sala <adriasala124@gmail.com>
2020-07-17 12:14:51 +0200

    Render page using markdown

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from . import util
 from django.http import Http404
+import markdown
 
 
 def index(request):
@@ -15,6 +16,9 @@ def wiki(request, title):
 
     if page == None:
         raise Http404("Wiki entry does not exist.")
+
+    md = markdown.Markdown()
+    page = md.convert(page)
 
     return render(request, "encyclopedia/wiki.html", {
         "page": page,
